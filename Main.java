576185bc761f795d81a4b0e2f8c9741cf895cb56
Izay0i <47558555+Izay0i@users.noreply.github.com>
2019-06-26 23:05:15 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -73,7 +73,7 @@ public class Main {
         System.out.println("-------");
 
         for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board.length; j++) {
+            for (int j = 0; j < board[i].length; j++) {
                 if (board[i][j] == 1)
                     System.out.print("|X");
                 else if (board[i][j] == 2)
